8d34aded85327ee1b016f434fbe239d141ceff50
Josué Ramos Souza <80490332+jramso@users.noreply.github.com>
2025-10-16 15:10:23 -0300

    meu primeiro commit

---
+++ Aula3.py
@@ -0,0 +1,12 @@
+lista = [[2,"B",2], [3,"B",3], [2,"B",2]] 
+# [2, "B", 2]
+# [3, "B", 3]
+# [2, "B", 2]
+lista2 = ['a', 'b', 'c']
+
+tamanho = (len(lista))
+contador = 0
+for linha in range(tamanho):
+    for coluna in range(tamanho):
+
+
